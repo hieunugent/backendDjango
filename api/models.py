4ada76d66410534bb21ffd151629f3901b8d6b7b
Henry Hieu Nguyen <hieunugent@gmail.com>
2024-08-13 23:55:14 -0700

    update

--- api/models.py
+++ api/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class User(models.Model):
@@ -8,4 +13,16 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
